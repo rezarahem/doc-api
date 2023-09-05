785ef2015ea7804fd398d8533313d4bdb4756437
reza_rahem <reza_rahem224@gmail.com>
2023-09-05 21:16:29 +0330

    fixing currentUser in roles.guard

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,19 +1,11 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  Session,
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '@prisma/client';
 import { ROLES_KEY } from 'src/decorators/roles.decorator';
-import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-  ) {}
+  constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
@@ -23,10 +15,10 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
+    const { currentUser } = context.switchToHttp().getRequest();
 
-    console.log(user);
+    console.log(currentUser);
 
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    return requiredRoles.some((role) => currentUser.roles?.includes(role));
   }
 }
